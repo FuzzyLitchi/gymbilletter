0fcc82a9299cba81737c99f5ca33ef832d653bbd
Polly Nielsen Boutet-Livoff <pollyboutet@gmail.com>
2018-05-13 23:32:35 +0200

    wild

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+#![feature(plugin, custom_derive)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+use rocket::request::Form;
+#[macro_use] extern crate diesel;
+use diesel::prelude::*;
+
+mod database;
+use database::{DbConn, init_pool};
+
+mod schema;
+mod party;
+
+#[get("/")]
+fn index(conn: DbConn) -> String {
+    use schema::parties::dsl::*;
+
+    let results = parties.select(title)
+        .load::<String>(&*conn)
+        .expect("Error");
+
+    format!("Hello World!\n{}", &results.join("\n"))
+}
+
+#[get("/party")]
+fn party() -> &'static str {
+    "Parties"
+}
+
+#[get("/party/<id>")]
+fn party_details(id: i32, conn: DbConn) -> Option<String> {
+    use schema::parties::dsl::parties;
+    use party::Party;
+
+    match parties.find(id)
+        .first::<Party>(&*conn) {
+        Ok(party) => Some(format!("Title: {}\nDescription: {}", party.title, party.body)),
+        Err(_) => None
+    }
+}
+
+use party::NewParty;
+#[post("/new_party", data = "<new_party>")]
+fn new_party(new_party: Form<NewParty>, conn: DbConn) -> String {
+    use schema::parties::dsl::parties;
+    use party::Party;
+
+    match diesel::insert_into(parties)
+        .values(new_party.get())
+        .get_result::<Party>(&*conn) {
+        Ok(party) => format!("Party \"{}\" has been created", party.title),
+        Err(_) => "Error".into(),
+    }
+}
+
+fn main() {
+    rocket::ignite()
+        .manage(init_pool())
+        .mount("/", routes![index])
+        .mount("/", routes![party])
+        .mount("/", routes![party_details])
+        .mount("/", routes![new_party])
+        .launch();
+}
---
+++ src/party.rs
@@ -0,0 +1,15 @@
+use super::schema::parties;
+
+#[derive(Queryable)]
+pub struct Party {
+    pub id: i32,
+    pub title: String,
+    pub body: String,
+}
+
+#[derive(Insertable, FromForm)]
+#[table_name="parties"]
+pub struct NewParty {
+    pub title: String,
+    pub body: String,
+}
