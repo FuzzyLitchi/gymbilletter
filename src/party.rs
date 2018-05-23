020b6c324e0500ededf36f40e8195cdc5b6947cc
Polly Nielsen Boutet-Livoff <pollyboutet@gmail.com>
2018-05-23 10:17:04 +0200

    Move party functions into the party module and same with user stuff

--- src/party.rs
+++ src/party.rs
@@ -1,5 +1,14 @@
 use super::schema::parties;
 
+use diesel;
+use diesel::prelude::*;
+
+use rocket::request::Form;
+use rocket_contrib::Template;
+
+use database::DbConn;
+use user::Admin;
+
 #[derive(Queryable, Serialize, Deserialize)]
 pub struct Party {
     pub id: i32,
@@ -12,4 +21,33 @@ pub struct Party {
 pub struct NewParty {
     pub title: String,
     pub body: String,
+}
+
+#[get("/")]
+fn list () -> &'static str {
+    "Parties"
+}
+
+#[get("/<id>")]
+fn details(id: i32, conn: DbConn) -> Option<String> {
+    match parties::dsl::parties.find(id)
+        .first::<Party>(&*conn) {
+        Ok(party) => Some(format!("Title: {}\nDescription: {}", party.title, party.body)),
+        Err(_) => None
+    }
+}
+
+#[get("/new")]
+fn new(user: Admin) -> Template {
+    Template::render("new_party", json!({"user": &user}))
+}
+
+#[post("/new", data = "<new_party>")]
+fn new_post(_user: Admin, new_party: Form<NewParty>, conn: DbConn) -> String {
+    match diesel::insert_into(parties::dsl::parties)
+        .values(new_party.get())
+        .get_result::<Party>(&*conn) {
+        Ok(party) => format!("Party \"{}\" has been created", party.title),
+        Err(_) => "Error".into(),
+    }
 }
